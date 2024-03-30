7175680bb3704778eddc37695bc8eeb2b008b513
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-03-30 14:16:50 -0400

    macos bugfix maybe?

---
+++ src/PlayLayer.cpp
@@ -0,0 +1,15 @@
+#include <Geode/modify/PlayLayer.hpp>
+#include "Manager.hpp"
+#include "Utils.hpp"
+
+using namespace geode::prelude;
+
+class $modify(MyPlayLayer, PlayLayer) {
+    bool canPauseGame() {
+        bool canPause = PlayLayer::canPauseGame();
+        if (canPause) {
+		    Manager::getSharedInstance()->channel->stop();
+        }
+        return canPause;
+    }
+};
